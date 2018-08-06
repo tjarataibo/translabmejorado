181a2c1a63481804d1f3549d7097e25010139266
Tamara <tjarataibo@gmail.com>
2018-08-06 11:42:50 -0400

    Quito tarjetas guardadas que no funcionan

--- src/js/perfil.js
+++ src/js/perfil.js
@@ -11,26 +11,3 @@ $(document).ready(function() {
 
 
 // Tarjetas guardadas
-let arrayTarjetas = [];
-btnAgregarTarjeta.addEventListener('click', agregarArray);
-
-function agregarArray() {
-  let elementoArray = numberTar.value;
-  arrayTarjetas.unshift(elementoArray);
-  console.log(arrayTarjetas);
-  numberTar.value = '';
-  // Pintar en pantalla las tarjetas guardadas
-  let tarjetaAlmacenada = document.createElement('input'); 
-  tarjetaAlmacenada.setAttribute('type', 'tel');
-  tarjetaAlmacenada.setAttribute('id', 'tarjetasGuardadas');
-  tarjetaAlmacenada.setAttribute('value', arrayTarjetas[0]);
-  document.body.appendChild(tarjetaAlmacenada);
-  // ..............................creando el Select de las tarjetas guardadas
-  let tarjetaGuardadaEnSelect = '';
-  tarjetaGuardadaEnSelect += '<select id="selectTarjeta">';
-  for (i = 0;i < arrayTarjetas.length;i++) {
-    tarjetaGuardadaEnSelect += `<option value="">${arrayTarjetas[i]}</option>`;
-  }
-  tarjetaGuardadaEnSelect += '</select>';
-  document.getElementById('tarjetas').innerHTML = tarjetaGuardadaEnSelect;
-}
