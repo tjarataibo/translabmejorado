f7c8c6726a8e43f1bbf906a198393e0afe9e7407
Tamara <tjarataibo@gmail.com>
2018-08-08 00:55:12 -0400

    Corrigiendo errores de test

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,4 +1,5 @@
-window.validateEmail = function enviarSubmit(evento) {
+window.validateEmail = (email) => {
+  console.log(email);
   if (email.indexOf('@') < 0) {
     return false;
   }
@@ -24,4 +25,38 @@ window.validateEmail = function enviarSubmit(evento) {
   }
 
   return true;
+};
+
+window.validatePassword = (password) => {
+  console.log(password);
+  if (password.toString().length <= 8) { // se comprueba el largo, maximo 8 
+    if (isNaN(password) === false) { // se comprueba que sean numeros
+      if (!password.trim() === ' ') { // se comprueba que no contenga espacios
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
+window.validateCard = (tarjeta) => {
+  console.log(tarjeta);
+  if (tarjeta.toString().length <= 10) {
+    if (isNaN(bip) === false) {
+      if (!tarjeta.toString().trim() === ' ') {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
